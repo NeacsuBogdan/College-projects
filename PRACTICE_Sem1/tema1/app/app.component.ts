7cafca4f9eba68fa0e7b4a7d3037ad55bff75859
NeacsuBogdan <117436637+NeacsuBogdan@users.noreply.github.com>
2022-12-09 17:02:50 +0200

    Add files via upload

---
+++ PRACTICE_Sem1/tema1/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lab2';
+  content = ["a","b","c"];
+  displayHeader: boolean = false;
+
+  changeHeader(){
+    this.displayHeader = !this.displayHeader;
+  }
+}
